2c97ba4600404fcb3ce78f6c0d4e45743f26613e
신석철 <scshin97@gmail.com>
2023-08-12 00:57:12 +0900

    Revert "feat : 토론방채팅기능구현중인데 중복으로 보내지고 불러와짐"
    This reverts commit a82bcb7ae9ae789a7bdfbb1e6876f8c16a8d203b

--- frontend/src/pages/debateRoom/components/TextChatting.jsx
+++ frontend/src/pages/debateRoom/components/TextChatting.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import SockJS from "sockjs-client";
-import Stomp from "webstomp-client"; // 올바르게 가져오기
-import { userInfoState } from "../../../recoil/userInfo"; // Make sure to import userInfoState from the correct path
+import webstomp from "webstomp-client";
+import { userInfoState } from "../../../recoil/userInfo";
 import style from "../debatePage.module.css";
 import { useRecoilValue } from "recoil";
 
@@ -14,50 +14,65 @@ function TextChatting({ roomId }) {
   const stompRef = useRef(null);
 
   useEffect(() => {
-    // const userNickname = userInfo.nickname;
+    const userNickname = userInfo.nickname;
+    // 웹 소켓 연결
     var sock = new SockJS("http://localhost:8081/mfc");
-    var stomp = Stomp.over(sock);
+    // var sock = new SockJS("https://goldenteam.site/mfc");
+    var stomp = webstomp.over(sock);
+    stompRef.current = stomp;
+    stomp.connect({}, function (frame) {
+    // 여기까지 연결 확인
 
-    stomp.connect({}, function () {
-      stompRef.current = stomp;
-      console.log("WebSocket connected!");
-
+      //4. subscribe(path, callback)으로 메세지를 받을 수 있음
       stomp.subscribe(`/from/chat/${roomId}`, (message) => {
-        const content = JSON.parse(message.body);
-        setChatMessages((prevMessages) => [...prevMessages, { sender: content.nickName, text: content.message }]);
+
+        // console.log('itwas',{message})
+        const content = message.body;
+        // console.log('itis',{content})
+        setChatMessages((prevMessage) => [...prevMessage, { sender: content.nickName, text: content.message }]);
       });
+
+      //3. send(path, header, message)로 메세지를 보낼 수 있음
+      stomp.send("/to/chat", {}, { roomId :roomId, nickName: userNickname, message: "테스트다" });
+      console.log(stomp.nickName)
     });
 
     return () => {
-      console.log("이건 되나?");
-      if (stompRef.current) {
-        stompRef.current.disconnect();
-        console.log("WebSocket disconnected!");
+      if (stomp.current) {
+        stomp.disconnect();
       }
-      console.log("WebSocket disconnected!"); // 연결 해제 확인을 위한 로그 출력
     };
   }, [roomId, userInfo.nickname]);
 
   const handleInputChange = (event) => {
+    // input 값이 입력될 때마다 갱신
     setInputText(event.target.value);
   };
 
   const handleSendMessage = () => {
-    if (stompRef.current && inputText.trim() !== "") {
+    if (inputText.trim() !== "") {
       stompRef.current.send(
         "/to/chat",
-        JSON.stringify({
-          roomId: `${roomId}`,
+        {},
+        {
+          roomId : roomId,
           nickName: userInfo.nickname,
           message: inputText,
-        })
+        }
       );
       setInputText("");
-      console.log("--------------");
-      console.log(chatMessages);
     }
   };
 
+  // // 메시지 받는 경우 테스트를 위한 함수
+  // const handleReceiveMessage = () => {
+  //     setChatMessages((prevMessage) => [
+  //         ...prevMessage,
+  //         {text: "메시지 받는거 테스트", sender: "other"},
+  //     ]);
+  // };
+
+  // 새 메시지가 추가될 때마다 스크롤을 맨 아래로 이동
   useEffect(() => {
     chatAreaRef.current.scrollTop = chatAreaRef.current.scrollHeight;
   }, [chatMessages]);
@@ -65,20 +80,22 @@ function TextChatting({ roomId }) {
   return (
     <>
       <div className={style.ChatArea}>
-        <div className={style.chatMessages} ref={chatAreaRef}>
-          {chatMessages.map((message, index) => (
-            <div
-              key={index}
-              className={`${style.messageContainer} ${
-                message.sender === userInfo.nickname ? style.userMessage : style.otherMessage
-              }`}
-            >
-              <p className={style.sender}>{message.sender === userInfo.nickname ? "나" : message.sender}</p>
-              <div className={style.messageBubble}>{message.text}</div>
+            <div className={style.chatMessages} ref={chatAreaRef}>
+                {chatMessages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`${style.messageContainer} ${
+                            message.sender === userInfo.nickname ? style.userMessage : style.otherMessage
+                        }`}
+                    >
+                        <p className={style.sender}>{message.sender === userInfo.nickname ? "나" : `${message.sender}`}</p>
+                        <p className={style.sender}>{message.sender === userInfo.nickname ? "나" : `${message.sender}`}</p>
+
+                        <div className={style.messageBubble}>{message.text}</div>
+                    </div>
+                ))}
             </div>
-          ))}
         </div>
-      </div>
       <div className={style.chatInput}>
         <input
           type="text"
@@ -90,6 +107,7 @@ function TextChatting({ roomId }) {
         <button className={style.button} onClick={handleSendMessage}>
           전송
         </button>
+        {/* <button onClick={handleReceiveMessage}>Receive Test</button> */}
       </div>
     </>
   );
